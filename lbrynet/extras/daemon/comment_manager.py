b4057e84c4ce442dd9413579bb892aabca93da0c
Oleg Silkin <o.silkin98@gmail.com>
2018-12-14 18:42:39 -0600

    Creates metadataclass
    Signed-off-by: Oleg Silkin <o.silkin98@gmail.com>

---
+++ lbrynet/extras/daemon/comment_manager.py
@@ -0,0 +1,9 @@
+import lbrynet.conf
+
+
+class MetadataServer:
+    """ We refer to the server that hosts all the comment data
+        and other good stuff as being the 'MetadataServer'. We use terminology
+        such as "claim data", but what we are in fact referring to is
+        the metadata that goes alongside the claims
+    """

651fd8cf351853bbc67cdeb87431f6ef7a1c7c4c
Oleg Silkin <o.silkin98@gmail.com>
2018-12-26 20:33:05 -0600

    Moves client from making synchronous calls to asynchronous using aiohttp
    Signed-off-by: Oleg Silkin <o.silkin98@gmail.com>

--- lbrynet/extras/daemon/comments/CommentClient.py
+++ lbrynet/extras/daemon/comments/CommentClient.py
@@ -1,16 +1,14 @@
-import requests
 import datetime
 from typing import Union
 import logging
-import asyncio
 import aiohttp
 
 log = logging.getLogger(__name__)
 
 
-class MetadataServer:
+class MetadataClient:
     """ We refer to the server that hosts all the comment data
-        and other good stuff as being the 'MetadataServer'. We use terminology
+        and other good stuff as being the 'MetadataClient'. We use terminology
         such as "claim data", but what we are in fact referring to is
         the metadata that goes alongside the claims. Said metadata comprises
         things such as likes or dislikes, and more importantly,
@@ -18,24 +16,20 @@ class MetadataServer:
     """
     __request_id: int = 0
 
-    def __init__(self,
-                 server_url: str = None,
-                 session: aiohttp.ClientSession = None,
-                 event_loop: asyncio.AbstractEventLoop = None,
-                 **kwargs):
+    def __init__(self, server_url: str = None,
+                 session: aiohttp.ClientSession = None, **kwargs):
         """
         :param server_url: Location of the server. Note that in the future
           this will be multiple, however for now it's just the one.
         :param session: This is the session object to be used for making
           HTTP requests to the actual server.
-        :param event_loop: This is the async event loop to be used when
-          invoking the actual
         """
         self._server_url: str = server_url
-        self._server_info: dict = {'last_updated': datetime.datetime.now(), 'status': None}
-        self._session = session if session is not None else aiohttp.ClientSession(
+        self.server_info: dict = {'last_updated': datetime.datetime.now(), 'status': None}
+        self.session = session if session is not None else aiohttp.ClientSession(
             headers={'Content-Type': 'application/json'}  # Sets headers here
         )
+        
         self._is_connected: bool = self.update_server_status()
 
     @property
@@ -48,7 +42,7 @@ class MetadataServer:
 
     @classmethod
     def _make_request_body(cls, method: str, params: dict = None) -> dict:
-        """ Creates a body for an HTTP request to the MetadataServer as
+        """ Creates a body for an HTTP request to the MetadataClient as
           documented. This method increments the request ID of the entire
           class after it generates the body.
 
@@ -78,21 +72,21 @@ class MetadataServer:
         :return: `dict` containing information about the server. `None` if
           we weren't able to communicate with server.
         """
-        return self._server_info['status']
+        return self.server_info['status']
 
     def update_server_status(self) -> bool:
         """ Tries to get the server's current status
         :return: True if the request succeeded
         """
         response = self.make_request('status')
-        self._server_info['last_updated'] = datetime.datetime.now()
+        self.server_info['last_updated'] = datetime.datetime.now()
         if response is None:
             return False
-        self._server_info['status'] = None if 'error' in response else response['result']
+        self.server_info['status'] = None if 'error' in response else response['result']
         return True
 
     async def make_request(self, method: str, params: dict = None,
-                     url: str = None, **kwargs) -> Union[dict, None]:
+                           url: str = None, **kwargs) -> Union[dict, None]:
         """ Asynchronously makes a request to the metadata server using the
         incremented ID, as well as the given method and parameters.
 
@@ -108,90 +102,21 @@ class MetadataServer:
         url = self._server_url if url is None else url
         body = self._make_request_body(method, params=params)
 
-        async with self._session as session:
+        async with self.session as session:
             try:
                 log.debug("Sending POST request to '%s' for method '%s'", url, method)
                 response = await session.post(url, json=body)
                 self._is_connected = True
-            except requests.exceptions.ConnectionError:
+            except aiohttp.ClientConnectionError:
                 self._is_connected = False
                 log.error("Failed to connect to '%s'", url)
                 return None
-        if not response.ok:
+        if not response.status < 400:
             log.error("Request [%i] got HTTP Error Code '%i' when connecting to '%s'",
-                      body['id'], response.status_code, url)
-            raise requests.HTTPError()
-        result = response.json()
-        return result
-
-
-''' ASYNC STUFF: Let's not use this until we have the normal sync version built
-
-class MetadataServer:
-    """ We refer to the server that hosts all the comment data
-        and other good stuff as being the 'MetadataServer'. We use terminology
-        such as "claim data", but what we are in fact referring to is
-        the metadata that goes alongside the claims. Said metadata comprises
-        things such as likes or dislikes, and more importantly,
-        the comments.
-    """
-    __request_id = 0
-    _session = aiohttp.ClientSession()
-
-    def __init__(self, server_url: str = None):
-        """
-        :param server_url: Location of the server. Note that in the future
-          this will be multiple, however for now it's just the one.
-        """
-        self._server_url = server_url
-        self._server_info = None
-
-    @property
-    def request_id(self):
-        self.__request_id += 1
-        return self.__request_id
-
-    @property
-    def server_url(self):
-        return self._server_url
-
-    @property
-    def status(self):
-
-
-    @classmethod
-    async def make_request(cls, url, method: str, params: dict = None):
-        """ Asynchronously makes a request to the metadata server using the
-        incremented ID, as well as the given method and parameters.
-
-        Note - The API's methods and parameters are documented online at [https://ocornoc.github.io/lbry-comments]
-
-        :param url: URL of the specific server the request will be amade to
-        :param method: API method to call from the comments server.
-        :param params: Parameters for the given method.
-        :return: The response received from the server
-        """
-        headers = {'Content-Type': 'application/json'}
-        body = {'jsonrpc': '2.0',
-                'method': method,
-                'id': cls.request_id}
-
-        if params is not None:
-            body['params'] = params
-
-        return await cls._session.post(url=url, headers=headers, json=body)
-
-
-async def server_status(session: aiohttp.ClientSession, url) -> dict:
-    headers = {"Content-Type": "application/json"}
-    body = {'jsonrpc': '2.0',
-            'method': 'status',
-            'id': '25'}
-    async with session.post(url=url, headers=headers, json=body) as response:
+                      body['id'], response.status, url)
+            raise aiohttp.http.HttpProcessingError(
+                code=response.status,
+                message=response.reason,
+                headers=response.headers
+            )
         return await response.json()
-
-async def main():
-    async with aiohttp.ClientSession() as session:
-        response = await server_status(session, "http://18.233.233.111:2903/api")
-        print(response)
-'''

84b22710da358de0837ed0c6425e7a007abf671b
Oleg Silkin <o.silkin98@gmail.com>
2018-12-28 02:31:39 -0600

    just whatever
    Signed-off-by: Oleg Silkin <o.silkin98@gmail.com>

--- lbrynet/extras/daemon/comments/MetadataManager.py
+++ lbrynet/extras/daemon/comments/MetadataManager.py
@@ -8,9 +8,13 @@ log = logging.getLogger(__name__)
 class MetadataManager:
     
     def __init__(self, server_url: str, username: str):
+        self.comments = {}
+        self.claim_metadata = {}
         self.username = username
         self._metadata_api = CommentsAPI(username=username, url=server_url)
     
     async def setup(self) -> dict:
         self._metadata_api.username = self.username
         return self._metadata_api.server.update_server_status()
+    
+    async def :

6a61d320d03ce0a5080914be70a12cf16f09c8c3
Oleg Silkin <o.silkin98@gmail.com>
2019-09-08 14:32:53 -0400

    Removes default `added_at` value & not-null restriction

--- lbry/lbry/extras/daemon/migrator/migrate11to12.py
+++ lbry/lbry/extras/daemon/migrator/migrate11to12.py
@@ -7,7 +7,7 @@ def do_migration(conf):
     connection = sqlite3.connect(db_path)
     connection.row_factory = sqlite3.Row
     cursor = connection.cursor()
-    cursor.executescript('alter table file add column added_at not null default (strftime(\'%s\', \'now\'))')
+    cursor.executescript('alter table file add column added_at')
     cursor.execute("UPDATE file SET added_at = strftime('%s', 'now')")
     connection.commit()
     connection.close()
